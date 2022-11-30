5b8cb3571ef59408099a70c58d6656e76a74c5b5
edenrosales <eden@rosales5.com>
2022-11-29 22:41:57 -0800

    Added formatting

--- components/TopBarStats.jsx
+++ components/TopBarStats.jsx
@@ -1,6 +1,38 @@
+import React, {Component} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+// import {styles} from 'C:\Users\eden\React Native\FinanceTracker\components\styles.jsx'
+
 class TopBarStats extends Component {
     render() {
-      return <Text>Hello, I am your cat!</Text>;
+      return (
+        <View>        
+          <View style = {styles.StatusBar}/>
+          <View style = {styles.TopBar}/>
+        </View>
+
+      )
+
     }
   }
+
+  const styles = StyleSheet.create({
+    TopBar: {
+      position: 'absolute',
+      backgroundColor: '#2C2E31',
+      height: '60%',
+      width: '100%',
+      top: 0,
+      bottom: 0,
+      left: 0, 
+      right: 0
+
+
+    },
+    StatusBar: { 
+      position: 'absolute',
+      backgroundColor: '#252628',
+      height: '9%'
+    }
+  });
 export default TopBarStats;
+
